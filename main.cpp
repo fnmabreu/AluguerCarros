ae5b98010028f4e5baa7901bde4c762fe5292468
fabio <fabio@alpha>
2014-01-19 22:28:03 +0000

    Projecto AluguerCarros
    C++/MYSQL

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: fabio
+ *
+ * Created on January 19, 2014, 7:56 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
